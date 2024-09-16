375fb331d6f114cf00e91c63248d7ec5fac8c695
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-09-16 20:15:30 +0300

    customise mail template.

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
@@ -15,10 +19,20 @@ class ForgotPasswordController extends Controller
     public function sendResetLinkEmail(Request $request)
     {
         $request->validate(['email' => 'required|email']);
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
-        return $status === Password::RESET_LINK_SENT ? back()->withSuccess($status) : back()->withErrors(["email" => $status]);
+        $user = User::where("email", $request->email)->first();
+        if (!$user) {
+            return back()->withErrors(["email" => "email bulunamadı"]);
+        }
+        $token = Password::createToken($user);
+        $resetUrl = route("password.reset", ["token" => $token, "email" => $user->email]);
+
+        $expires = Carbon::now()->addMinutes(60);
+
+        Mail::send("mail.reset", ["resetUrl" => $resetUrl, "expires" => $expires], function (Message $message) use ($request) {
+            $message->to($request->email)->subject("Sıfırlama Maili");
+        });
+        return back()->withSuccess("Sıfırlama Maili Gönderildi");
+
     }
 
     public function showResetForm(Request $request, $token)
---
+++ resources/views/mail/reset.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Şifre Sıfırla</title>
+</head>
+<body>
+<h1>Şifre Sıfırla</h1>
+<p>Merhaba,</p>
+<p>Özelleştirilmiş mail : <a href="{{$resetUrl}}">Şifre Sıfırlamak İçin Tıkla.</a></p>
+<p>Şifre Sıfırlama süresi : {{$expires}}</p>
+<p>Teşekkürkler.</p>
+</body>
+</html>
